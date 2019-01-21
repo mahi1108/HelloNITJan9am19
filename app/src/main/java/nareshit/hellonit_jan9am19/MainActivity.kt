12ae28267999e908ffa9f4daad85c8e995de27ea
maheshthippala <mahesh.cubex@gmail.com>
2019-01-21 10:01:06 +0530

    Initial commit

---
+++ app/src/main/java/nareshit/hellonit_jan9am19/MainActivity.kt
@@ -0,0 +1,13 @@
+package nareshit.hellonit_jan9am19
+
+import android.os.Bundle
+
+class MainActivity:android.app.Activity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
